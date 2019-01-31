b0670bde651fbfc84f17ff73c8d0958b6d2b7e0c
Roberto Panetta <0639300@johnabbottcollege.net>
2019-01-31 08:39:55 -0500

    Class 2 Example

---
+++ Class2Example1/Class2Example1/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Class2Example1.Default" Trace="true" %>
+
+<%
+    this.HelloWorldLabel.Text = "This is from the script block";
+%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="HelloWorldLabel" runat="server"></asp:Label>
+            <br /><br />
+            <asp:TextBox ID="TextInput" runat="server"></asp:TextBox>
+
+            <asp:Button ID="GreetButton" runat="server" Text="Say Hello!" OnClick="GreetButton_Click" />
+            <br /><br />
+            <asp:DropDownList ID="GreetList" runat="server" AutoPostBack="true" OnSelectedIndexChanged="GreetList_SelectedIndexChanged">
+                <asp:ListItem Value="no one">No one</asp:ListItem>
+                <asp:ListItem Value="world">World</asp:ListItem>
+                <asp:ListItem Value="universe">Universe</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Class2Example1/Class2Example1/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace Class2Example1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            //HelloWorldLabel.Text = "From PageInit";
+        }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            HelloWorldLabel.Text = "From PageInitComplete";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HelloWorldLabel.Text = "From PageLoad";
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            HelloWorldLabel.Text = "From PagePreRender";
+        }
+
+        protected void Page_Render(object sender, EventArgs e)
+        {
+            HelloWorldLabel.Text = "From PageRender";
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            HelloWorldLabel.Text = "From PageUnload";
+        }
+
+        protected void GreetButton_Click(object sender, EventArgs e)
+        {
+            //Copy the text from TextBox into the Label
+            if(String.IsNullOrEmpty(this.TextInput.Text))
+            {
+                this.HelloWorldLabel.Text = "Hello, from default value!";
+            }
+            else
+            {
+                this.HelloWorldLabel.Text = this.TextInput.Text;
+            }
+
+        }
+
+        protected void GreetList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Copy the value of the selected item into the Label
+            //this.HelloWorldLabel.Text = "Hello from " + this.GreetList.SelectedValue;
+            this.HelloWorldLabel.Text = String.Format("Hello from {0}", this.GreetList.SelectedValue);
+        }
+    }
+}
