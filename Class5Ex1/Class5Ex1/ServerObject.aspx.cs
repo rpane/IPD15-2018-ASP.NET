653984bd77b3bff55c2ff9e247a027dc462b07ce
Roberto Panetta <0639300@johnabbottcollege.net>
2019-02-01 10:54:39 -0500

    ..

---
+++ Class5Ex1/Class5Ex1/ServerObject.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Class5Ex1
+{
+    public partial class SessionObject : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
