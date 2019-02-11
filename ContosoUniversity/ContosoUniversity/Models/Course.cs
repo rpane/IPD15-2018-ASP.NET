59bdc17c4b46912813e20438b27c29cda9ace890
Roberto Panetta <0639300@johnabbottcollege.net>
2019-02-11 13:01:23 -0500

    ..

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public  string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+                
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
