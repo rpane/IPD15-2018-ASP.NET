6ffe8f478e64e96536f993d5fb486bdd35e11666
Roberto Panetta <0639300@johnabbottcollege.net>
2019-02-12 12:06:11 -0500

    ..

---
+++ ContosoUniversity/ContosoUniversity/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ContosoUniversity.Models.ContosoUniversityContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ContosoUniversity.Models.ContosoUniversityContext";
+        }
+
+        protected override void Seed(ContosoUniversity.Models.ContosoUniversityContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
