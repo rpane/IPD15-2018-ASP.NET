28ca7f2ffefb8281ff1139b183b4b74a1bdf3dce
Roberto Panetta <0639300@johnabbottcollege.net>
2019-02-14 12:34:05 -0500

    ...

---
+++ MVCCoreMovie/MVCCoreMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCCoreMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
